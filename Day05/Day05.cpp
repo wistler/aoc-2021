c6a135cfdd794f41c08cdca0e40991e53e7c31cf
Sooraj Kumar <the.wistler@gmail.com>
2021-12-05 10:29:28 -0500

    cleanup

--- Day05/Day05.cpp
+++ Day05/Day05.cpp
@@ -1,5 +1,4 @@
 ﻿#include "Day05.h"
-#include <tuple>
 
 using namespace std;
 
@@ -44,17 +43,20 @@ vector<Line> getLines(istream &ss) {
 	return lines;
 }
 
-void plotCoord(map<Coord, int>& m, Coord c) {
+inline void plotCoord(map<Coord, int>& m, Coord c) {
 	m[c]++;
-	// cout << "Coord: " << c.str() << " -> " << m[c] << endl;
 }
 
-void plotLine(map<Coord, int>& m, Line& li, bool includeDiag) {
-	bool isDiag = li.x1 != li.x2 && li.y1 != li.y2;
+inline int getStep(int x1, int x2) {
+	return x2 - x1 == 0 ? 0 : x2 - x1 > 0 ? +1 : -1;
+}
+
+void plotLine(map<Coord, int>& m, Line& li, const bool includeDiag) {
+	const bool isDiag = li.x1 != li.x2 && li.y1 != li.y2;
 
 	if (!isDiag || includeDiag) {
-		int dx = li.x2 - li.x1 == 0 ? 0 : li.x2 - li.x1 > 0 ? +1 : -1;
-		int dy = li.y2 - li.y1 == 0 ? 0 : li.y2 - li.y1 > 0 ? +1 : -1;
+		const int dx = getStep(li.x1, li.x2);
+		const int dy = getStep(li.y1, li.y2);
 		for (int x = li.x1, y = li.y1; !(x == li.x2 && y == li.y2); x += dx, y += dy) {
 			plotCoord(m, Coord{ x, y });
 		}
@@ -62,17 +64,30 @@ void plotLine(map<Coord, int>& m, Line& li, bool includeDiag) {
 	}
 }
 
-map<Coord, int> plot(vector<Line>& lines, bool includeDiag) {
+map<Coord, int> plot(vector<Line>& lines, const bool includeDiag) {
 	map<Coord, int> m;
 	for each (Line li in lines) {
-		// cout << "Plotting: " << li.str() << endl;
 		plotLine(m, li, includeDiag);
 	}
-	
 	return m;
 }
 
-void parseInput(istream& ss) {
+void drawMap(map<Coord, int>& map) {
+	int minX{0}, maxX{0}, minY{0}, maxY{0};
+	for (auto const& kv : map) {
+		Coord c = kv.first;
+		if (minX > c.x) minX = c.x;
+		if (maxX < c.x) maxX = c.x;
+		if (minY > c.y) minY = c.y;
+		if (maxY < c.y) maxY = c.y;
+	}
+	for (int y = minY; y <= maxY; y++)
+		for (int x = minX; x <= maxX; x++)
+			cout << (map[Coord{ x, y }] ? to_string(map[Coord{ x, y }]) : ".") 
+				 << (x == maxX ? "\n" : "");
+}
+
+void parseInput(istream& ss, const bool debug) {
 	vector<Line> lines = getLines(ss);
 
 	map<Coord, int> map = plot(lines, false);	
@@ -81,6 +96,7 @@ void parseInput(istream& ss) {
 		if (x.second > 1) dangerous++;
 	}
 	cout << "Part 1 | Dangerous Spots = " << dangerous << endl;
+	if (debug) drawMap(map);
 
 	map = plot(lines, true);
 	int allDangerous = 0;
@@ -88,6 +104,7 @@ void parseInput(istream& ss) {
 		if (x.second > 1) allDangerous++;
 	}
 	cout << "Part 2 | Dangerous Spots = " << allDangerous << endl;
+	if (debug) drawMap(map);
 }
 
 void main()
@@ -110,10 +127,10 @@ void main()
 
 	stringstream testDataStream(testData);
 	cout << "With Test Data .." << endl;
-	parseInput(testDataStream);
+	parseInput(testDataStream, true);
 
 	cout << endl << "With Input Data .." << endl;
 	ifstream input = get_input_file(DAY + "/input.txt");
-	parseInput(input);
+	parseInput(input, false);
 	
 }
